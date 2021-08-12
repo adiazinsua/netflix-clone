7dc318ed285a4d1aa2fbaa57b8897d7c61be876e
Ailín Díaz Insua <ardiazinsua@gmail.com>
2021-08-12 00:12:06 -0300

    smalls css changes

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -124,6 +124,25 @@ export const SearchIcon = styled.button`
   }
 `;
 
+export const PlayButton = styled.button`
+  box-shadow: 0 0.6vw 1vw -0.4vw rgba(0, 0, 0, 0.35);
+  background-color: #e6e6e6;
+  color: #000;
+  border-width: 0;
+  padding: 10px 20px;
+  border-radius: 5px;
+  max-width: 130px;
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 30px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #ff1e1e;
+    color: white;
+  }
+`;
+
 export const SearchInput = styled.input`
 background-color: #44444459;
 color:whitel
@@ -131,6 +150,11 @@ border: 1px solid white;
 transition width 0.5s;
 height: 30px;
 font-size: 14px;
+margin-left: ${({ active }) => (active === true ? "10px" : "0")}; 
+padding: ${({ active }) => (active === true ? "0 10px" : "0")}; 
+opacity: ${({ active }) => (active === true ? "1" : "0")}; 
+width: ${({ active }) => (active === true ? "200px" : "0px")}; 
+
 `;
 
 export const Profile = styled.div`
--- src/containers/browse.js
+++ src/containers/browse.js
@@ -63,6 +63,7 @@ export const BrowseContainer = ({ slides }) => {
             he projects in a futile attempt to feel like he's part of the world
             around him.
           </Header.Text>
+          <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
     </>
